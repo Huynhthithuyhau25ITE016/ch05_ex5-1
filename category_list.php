b81df00662ead43f248371b0e79673bcacef853f
Huynhthithuyhau25ITE016 <hauhtt.25ite@vku.udn.vn>
2026-09-25 09:18:58 +0700

    Step 8: Add category form
    Added a form to submit a new category with a name.

--- category_list.php
+++ category_list.php
@@ -23,7 +23,14 @@
     </table>
 
     <h2>Add Category</h2>
-    <!-- add code for form here -->
+    <form action="index.php" method="post">
+    <input type="hidden" name="action" value="add_category">
+
+    <label>Name:</label>
+    <input type="text" name="category_name">
+
+    <input type="submit" value="Add">
+</form>
 
     <p><a href="index.php?action=list_products">List Products</a></p>
 
