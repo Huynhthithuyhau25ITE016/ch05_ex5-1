9e5c574046faafae100300a4f426d945e2eb784e
Huynhthithuyhau25ITE016 <hauhtt.25ite@vku.udn.vn>
2026-09-25 10:02:04 +0700

    Step 15: Refactor category navigation

--- category_list.php
+++ category_list.php
@@ -1,38 +1,22 @@
 <?php include '../view/header.php'; ?>
 <main>
-
-    <h1>Category List</h1>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>&nbsp;</th>
-        </tr>        
-        <?php foreach ($categories as $category) : ?>
-<tr>
-    <td><?php echo $category['categoryName']; ?></td>
-    <td>
-        <form action="index.php" method="post">
-            <input type="hidden" name="action" value="delete_category">
-            <input type="hidden" name="category_id"
-                   value="<?php echo $category['categoryID']; ?>">
-            <input type="submit" value="Delete">
-        </form>
-    </td>
-</tr>
-<?php endforeach; ?>
-    </table>
-
-    <h2>Add Category</h2>
-    <form action="index.php" method="post">
-    <input type="hidden" name="action" value="add_category">
-
-    <label>Name:</label>
-    <input type="text" name="category_name">
-
-    <input type="submit" value="Add">
-</form>
-
-    <p><a href="index.php?action=list_products">List Products</a></p>
-
+    <aside>
+        <h1>Categories</h1>
+        <?php include '../view/categories_nav.php'; ?>
+    </aside>
+    <section>
+        <h1><?php echo $category_name; ?></h1>
+        <ul class="nav">
+            <!-- display links for products in selected category -->
+            <?php foreach ($products as $product) : ?>
+            <li>
+                <a href="?action=view_product&amp;product_id=<?php 
+                          echo $product['productID']; ?>">
+                    <?php echo $product['productName']; ?>
+                </a>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
 </main>
 <?php include '../view/footer.php'; ?>
